a154a14a85d7d0775221784d983911848718f257
Jessica <jessicaguevara193@gmail.com>
2023-06-22 01:19:22 -0400

    added functions for the des and amt

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -1,11 +1,37 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 
 
 const Credit = ({credit, setCredit, accountBalance}) => {
+
+  const [inputDes, setInputDes] = useState('');
+  const[inputAmt, setInputAmt] = useState(0);
+
+  function handleInputDes(event){
+      setInputDes(event.target.value)
+  }
+
+  function handleInputAmt(event){
+      setInputAmt(event.target.value)
+  }
+
+  function updateAmt(event){
+      event.preventDefault();
+      //check to make sure there are no empty strings
+      let updatedAmt ={
+        "description": inputDes,
+        "amount": inputAmt
+      }
+  }
 console.log(credit)
   return (
     <>
-    <div id = 'content'>
+    <div id = 'credit'>
+    <h1>Add Debit</h1>
+     <p>Description</p>
+     <input type="text" value={inputDes} onChange={handleInputDes} />
+     <p>Add Amount:</p>
+      <input type="number" value={inputAmt} onChange={handleInputAmt} />
+      <button onClick={updateAmt}>Submit</button>
       <h1>Current Credit:</h1>
         <h3>$ {credit}</h3>
     </div>
--- src/components/Debit.jsx
+++ src/components/Debit.jsx
@@ -1,10 +1,36 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 
 const Debit=({debit, setDebit, accountBalance}) => {
+    const [inputDes, setInputDes] = useState('');
+    const[inputAmt, setInputAmt] = useState(0);
+
+    function handleInputDes(event){
+        setInputDes(event.target.value)
+    }
+
+    function handleInputAmt(event){
+        setInputAmt(event.target.value)
+    }
+
+    function updateAmt(event){
+        event.preventDefault();
+        //check to make sure there are no empty strings
+        let updatedAmt ={       
+        "description": inputDes,
+        "amount": inputAmt
+       // debit: debit+inputAmt
+        }
+    }
 
   return (
     <>
-    <div id = 'content'>
+    <div id = 'debit'>
+     <h1>Add Debit</h1>
+     <p>Description</p>
+     <input type="text" value={inputDes} onChange={handleInputDes} />
+     <p>Add Amount:</p>
+      <input type="number" value={inputAmt} onChange={handleInputAmt} />
+      <button onClick={updateAmt}>Submit</button>
      <h1>Current Debit:</h1>
         <h3>$ {debit}</h3>
     </div>
@@ -13,3 +39,5 @@ const Debit=({debit, setDebit, accountBalance}) => {
 }
 
 export default Debit;
+//i might change it to a forms
+
